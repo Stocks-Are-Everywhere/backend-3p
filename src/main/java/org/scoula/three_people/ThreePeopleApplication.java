52ad8d764ab47d381a2b409609fd8191afecde8b
Hyemin Lee <hmin10580@gmail.com>
2025-02-10 14:03:38 +0900

    Initial commit

--- src/main/java/org/scoula/three_people/ThreePeopleApplication.java
+++ src/main/java/org/scoula/three_people/ThreePeopleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoonTwoPeopleApplication {
+public class ThreePeopleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SoonTwoPeopleApplication.class, args);
+        SpringApplication.run(ThreePeopleApplication.class, args);
     }
 
 }
