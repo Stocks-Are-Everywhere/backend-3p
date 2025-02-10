134b51aa47f44e6be1cc050be93672daa69ac0f1
Hyemin Lee <hmin10580@gmail.com>
2025-02-10 17:05:17 +0900

    Add repository skeleton

--- src/main/java/org/scoula/three_people/member/repository/AccountRepositoryImpl.java
+++ src/main/java/org/scoula/three_people/member/repository/AccountRepositoryImpl.java
@@ -1,8 +1,11 @@
 package org.scoula.three_people.member.repository;
 
+import org.scoula.three_people.member.domain.Account;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -10,4 +13,7 @@ public class AccountRepositoryImpl {
 
 	private final AccountJpaRepository accountJpaRepository;
 
+	public Optional<Account> findById(Long id) {
+		return accountJpaRepository.findById(id);
+	}
 }
