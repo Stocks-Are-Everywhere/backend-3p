a8e9e947a6239564c7224e9a3226271ce1df8a3c
김연진 <love_homebody@naver.com>
2025-02-17 09:20:56 +0900

    test: matching with unfilled market orders for limit orders

--- src/test/java/org/scoula/three_people/order/service/datastructure/StockOrderBookTest.java
+++ src/test/java/org/scoula/three_people/order/service/datastructure/StockOrderBookTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.scoula.three_people.order.domain.Order;
 import org.scoula.three_people.order.domain.OrderHistory;
@@ -62,10 +63,58 @@ class StockOrderBookTest {
 		assertThat(response).hasSize(1);
 	}
 
+	@Test
+	@DisplayName("존재하는 시장가 주문을 모두 소진한다.")
+	void matchBuyOrderWithMarketOrderFirst() {
+		// given
+		stockOrderBook.matchFixedPrice(generateMarketOrder(Type.SELL, 2, 1L));
+		stockOrderBook.matchFixedPrice(generateMarketOrder(Type.SELL, 2, 2L));
+		Order buy = generateOrder(Type.BUY, 6, 4L);
+
+		// when
+		List<OrderHistory> marketOrder = stockOrderBook.matchWithMarketOrder(buy);
+
+		// then
+		assertThat(marketOrder).hasSize(2);
+		marketOrder
+			.forEach(orderHistory -> assertThat(orderHistory.getPrice()).isEqualTo(5000));
+	}
+
+	@Test
+	@DisplayName("존재하는 시장가 주문을 모두 소진한다.")
+	void matchSellOrderWithMarketOrderFirst() {
+		// given
+		stockOrderBook.matchFixedPrice(generateMarketOrder(Type.BUY, 2, 1L));
+		stockOrderBook.matchFixedPrice(generateMarketOrder(Type.BUY, 2, 2L));
+		Order buy = generateOrder(Type.SELL, 6, 4L);
+
+		// when
+		List<OrderHistory> marketOrder = stockOrderBook.matchWithMarketOrder(buy);
+
+		// then
+		assertThat(marketOrder).hasSize(2);
+		marketOrder
+			.forEach(orderHistory -> assertThat(orderHistory.getPrice()).isEqualTo(5000));
+	}
+
 	Order generateOrder(Type type, int quantity, Long id) {
 		return Order.builder()
+			.id(id)
 			.companyCode("companyCode")
 			.price(5000)
+			.totalQuantity(quantity)
+			.remainingQuantity(quantity)
+			.status(OrderStatus.ACTIVE)
+			.type(type)
+			.createdDateTime(LocalDateTime.of(2025, 2, 16, 6, 21))
+			.build();
+	}
+
+	Order generateMarketOrder(Type type, int quantity, Long id) {
+		return Order.builder()
+			.id(id)
+			.companyCode("companyCode")
+			.price(0)
 			.totalQuantity(quantity)
 			.remainingQuantity(quantity)
 			.status(OrderStatus.ACTIVE)
